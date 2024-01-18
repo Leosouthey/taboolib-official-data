89ff3eed430b0de07ce60a04100d0c3b809bb76c
Leosouthey <621862169@qq.com>
2024-01-19 03:02:30 +0800

    Remove unused imports from object.ts

--- app/utils/object.ts
+++ app/utils/object.ts
@@ -1,7 +1,3 @@
-// import platformsCategories from "@/content/platforms/categories.json";
-// import modulesCategories from "@/content/modules/categories.json";
-// import expansionsCategories from "@/content/expansions/categories.json";
-// import templatesCategories from "@/content/templates/categories.json";
 import componentTypes from "@/content/types.json";
 import hots from "@/content/hots.json";
 import matter from "gray-matter";
